5c96320f1d69ebb893d533282566aba43df0e7d2
Osama Ayub <ayubosama416@gmail.com>
2025-05-11 02:04:13 +0500

    added a pagination component

--- components/jobs-list.tsx
+++ components/jobs-list.tsx
@@ -2,10 +2,11 @@
 import { useState, useEffect } from "react";
 
 import axios from "axios";
-import { Button } from "@/components/ui/button";
+
 
 import { Filters } from "@/types/filter";
 import { JobCard } from "./job-card";
+import { Pagination } from "./pagination-card";
 
 
 export function JobsList({ filters }: { filters: Filters }) {
@@ -69,48 +70,4 @@ export function JobsList({ filters }: { filters: Filters }) {
 
 
 
-// Pagination Component
-function Pagination({
-  currentPage,
-  totalPages,
-  onPageChange,
-}: {
-  currentPage: number;
-  totalPages: number;
-  onPageChange: (page: number) => void;
-}) {
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-  return (
-    <div className="flex justify-center items-center flex-wrap gap-2 mt-6">
-      <Button
-        size="sm"
-        variant="outline"
-        disabled={currentPage === 1}
-        onClick={() => onPageChange(currentPage - 1)}
-      >
-        Previous
-      </Button>
-
-      {pages.map((page) => (
-        <Button
-          key={page}
-          size="sm"
-          variant={currentPage === page ? "default" : "outline"}
-          onClick={() => onPageChange(page)}
-        >
-          {page}
-        </Button>
-      ))}
-
-      <Button
-        size="sm"
-        variant="outline"
-        disabled={currentPage === totalPages}
-        onClick={() => onPageChange(currentPage + 1)}
-      >
-        Next
-      </Button>
-    </div>
-  );
-}
---
+++ components/pagination-card.tsx
@@ -0,0 +1,48 @@
+
+import {Button} from "@/components/ui/button";
+
+export // Pagination Component
+function Pagination({
+    currentPage,
+    totalPages,
+    onPageChange,
+  }: {
+    currentPage: number;
+    totalPages: number;
+    onPageChange: (page: number) => void;
+  }) {
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  
+    return (
+      <div className="flex justify-center items-center flex-wrap gap-2 mt-6">
+        <Button
+          size="sm"
+          variant="outline"
+          disabled={currentPage === 1}
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          Previous
+        </Button>
+  
+        {pages.map((page) => (
+          <Button
+            key={page}
+            size="sm"
+            variant={currentPage === page ? "default" : "outline"}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </Button>
+        ))}
+  
+        <Button
+          size="sm"
+          variant="outline"
+          disabled={currentPage === totalPages}
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          Next
+        </Button>
+      </div>
+    );
+  }
