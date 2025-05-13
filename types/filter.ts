8d6fe4e87240ea183b91a948c57386378901c2a0
Osama Ayub <ayubosama416@gmail.com>
2025-05-13 21:30:01 +0500

    Update filter.ts

--- types/filter.ts
+++ types/filter.ts
@@ -31,13 +31,6 @@ import { Key } from "react";
   }
 
 
-export interface Filters {
-    category_name: string
-    country: string
-    salary: number[]
-    title:string,
-    organization:string,
-    city:string
-  }
+
 
 
